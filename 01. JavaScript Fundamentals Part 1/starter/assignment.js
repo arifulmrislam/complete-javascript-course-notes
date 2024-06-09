1107c5e2c7516f25ee8b49fa81edbdaa14f8a002
arifulmrislam <ariful.mr.islam@gmail.com>
2024-06-09 15:21:18 +0600

    first commit🔥

---
+++ 01. JavaScript Fundamentals Part 1/starter/assignment.js
@@ -0,0 +1,112 @@
+// Lecture: Values and Variables
+
+// let country = "Bangladesh";
+// let continent = "Asia";
+// let population = 10;
+
+// // console.log(country, continent, population);
+
+// // Lecture: Data types
+// const isIsland = false;
+// const language = "Bangla";
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+// Lecture: let, const and var
+// language = "English";
+
+// console.log(language);
+
+// Lecture: Basic operators
+
+// console.log(population/2);
+
+// let finland = 6;
+
+// console.log(population - finland);
+
+// let average_country = 33;
+// if(average_country > population){
+//     // console.log(true);
+// }
+
+// let description = `${country} is in ${continent}, and its ${population} million people speak ${language}.`;
+
+// Lecture: String and Template Literals
+// console.log(description);
+
+// Lecture: Taking Decisions: if / else Statements
+
+
+// Lecture: Type conversion and coercion
+
+// console.log("9" - "5"); 
+// console.log('19' - '13' + '17');
+// console.log('19' - '13' + 17);
+// console.log('123' < 57);
+// console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+// Lecture: Equality Operators: == vs ===
+
+// let numNeighbours = prompt ('How many neighhbour countries does your country have?');
+
+// if(Number(numNeighbours) == 1){ 
+//     console.log('Only one Border')
+// }else if(numNeighbours > 1){
+//     console.log('More than 1 border')
+// }else{
+//     console.log('No border')
+// }
+
+// Lecture: Logical Operators
+
+// let country = "England";
+// let continent = "Asia";
+// let population = 30;
+// const isIsland = false;
+// const language = "Bangla";
+
+// if(country === "England"){
+//     console.log (`You should live in ${country}`);
+// }else if(isIsland === false){
+//     console.log(`You should live in ${country}`);
+// }else if(population < 50){
+//     console.log(`You should live in ${country}`);
+// }else{
+//     console.log(`${country} does not meet your criteria`);
+// }
+
+// Switch statement
+// let language = 'english';
+// switch(language){
+//     case 'chinese':
+//     case 'mandarin':
+//         console.log('Most number of native speaker');
+//         break;
+//     case 'spanish':
+//         console.log('2nd place in number of native speaker');
+//         break;
+//     case 'english':
+//         console.log('3rd place');
+//         break;
+//     case 'hindi':
+//         console.log('Number 4');
+//         break;
+//     case 'arabic':
+//         console.log('5th most spoken language');
+//         break;
+//     default:
+//         console.log('Great language too :D');
+// }
+
+// Lecture: The conditional (Ternary) operator
+// let country = "England";
+// let continent = "Asia";
+// let population = 130;
+// const isIsland = false;
+// const language = "Bangla";
+
+// console.log( population > 33 ? "Portugal's population is above average" : "Portugal's population is below average" );
---
+++ 01. JavaScript Fundamentals Part 1/starter/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+countries does your country have?');
+
+if(numNeighbours == 1){ 
+    console.log('Only one Border')
+}else if{
+    console.log('More than 1 border')
+}else{
+    console.log('No border')
+}
