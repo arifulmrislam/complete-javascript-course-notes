fcbe8c6768ee8830bcd4aca24aa92d9ffb3c0cb3
arifulmrislam <ariful.mr.islam@gmail.com>
2025-05-17 08:25:01 -0500

    pic game last part

--- 05. DOM and Events Fundamentals/07-Pig-Game/starter/script.js
+++ 05. DOM and Events Fundamentals/07-Pig-Game/starter/script.js
@@ -12,40 +12,87 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-//Starting condition
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-let currentScore = 0;
-let activePlayer = 0;
+//Starting condition
+const init = function() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+
+    player0El.classList.remove('player--active');
+    player1El.classList.remove('player--active');
+}
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+};
+init();
 
 //Rolling dice functionality
-btnNew.addEventListener('click', function(){
-    console.log('clicked');
-});
-btnRoll.addEventListener('click', function(){
-    // console.log('clicked');
-    //1. Generating a random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    //2. Display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
-    // console.log(dice);
-    //3. Check for rolled 1: if true, switch to next player
-    if(dice !== 1) {
-        //Add dice to current score
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    } else {
-        //Switch to next player
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
+
+btnRoll.addEventListener('click', function () {
+    if (playing) {
+        // console.log('clicked');
+        //1. Generating a random dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
+        //2. Display dice
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;
+        // console.log(dice);
+        //3. Check for rolled 1: if true, switch to next player
+        if (dice !== 1) {
+            //Add dice to current score
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent =
+            currentScore;
+        } else {
+            switchPlayer();
+        }
     }
 });
-btnHold.addEventListener('click', function(){
-    console.log('clicked');
+
+btnHold.addEventListener('click', function () {
+    if (playing) {
+        //1. Add current score to active player's score
+        scores[activePlayer] += currentScore;
+        // console.log(scores[activePlayer]);
+        //scores[1] = scores[1] + currentScore
+        document.getElementById(`score--${activePlayer}`).textContent =
+            scores[activePlayer];
+        //2. Check if player's score is >= 100
+        if (scores[activePlayer] >= 20) {
+            // Finish the game
+            playing = false;
+            diceEl.classList.add('hidden');
+
+            document
+                .querySelector(`.player--${activePlayer}`)
+                .classList.add('player--winner');
+                document
+                .querySelector(`.player--${activePlayer}`)
+                .classList.remove('player--active');
+        } else {
+            switchPlayer();
+        }
+        // Switch to the next player
+    }
 });
+
+btnNew.addEventListener('click', init);
